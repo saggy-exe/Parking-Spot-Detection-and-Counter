b60b2acae79d5fb6f7f9fd6366b708161da32c53
saggy-exe <ghoshsagnic04@gmail.com>
2025-01-30 01:38:47 +0530

    going in

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 from util import get_parking_spots_bboxes, empty_or_not
 
 def calc_diff(im1, im2):
@@ -13,7 +12,7 @@ video_path = "./data/parking_1920_1080_loop.mp4"
 mask = cv2.imread(mask, 0)
 cap = cv2.VideoCapture(video_path)
 
-connected_components = cv2.connectedComponentsWithStats(mask, 4, cv2.CV_32S)
+connected_components = cv2.connectedComponentsWithStats(mask,4, cv2.CV_32S)
 
 spots = get_parking_spots_bboxes(connected_components)
 
@@ -26,7 +25,6 @@ ret = True
 step = 30
 while ret:
     ret, frame = cap.read()
-
     if frame_nbr % step == 0 and previous_frame is not None:
         for spot_index, spot in enumerate(spots):
             x1, y1, w, h = spot
@@ -37,11 +35,15 @@ while ret:
 
 
         print([diffs[j] for j in np.argsort(diffs)][::-1])
-        plt.hist([diffs[j] / np.amax(diffs) for j in np.argsort(diffs)][::-1])
-        plt.show()
 
     if frame_nbr % step == 0:
-        for spot_index, spot in enumerate(spots):
+        if previous_frame is None:
+            arr_ = range(len(spots))
+        else:
+            arr_ = [j for j in np.argsort(diffs) if diffs[j] / np.amax(diffs)][::-1]
+        for spot_index in arr_:
+            spot = spots[spot_index]
+
             x1, y1, w, h = spot
 
             spot_crop = frame[y1:y1 + h, x1:x1 + w, :]
@@ -60,6 +62,9 @@ while ret:
             frame = cv2.rectangle(frame, (x1, y1), (x1 + w, y1 + h), (0, 255, 0), 2)
         else:
             frame = cv2.rectangle(frame, (x1, y1), (x1 + w, y1 + h), (0, 0, 255), 2)
+
+    cv2.putText(frame, 'Available spots: {} / {}'.format(str(sum(spots_status)), str(len(spots_status))), (100, 60),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
 
     cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
     cv2.imshow('frame', frame)
